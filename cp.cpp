20016bf85b3227098444a80ca2d7528836fde631
ABHAY VAISHNAV <abhayvaishnav551@gmail.com>
2022-06-26 11:10:46 +0530

    Add files via upload
    1st commit

---
+++ cp.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// void solve() {
+
+// }
+
+
+
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    cout << "Abhay";
+
+    return 0;
+}
